ff96ad9ccad3abe35b1beede8ef4fe0ad82c78da
astap-bender <150295041+astap-bender@users.noreply.github.com>
2025-07-24 20:13:11 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './main.css'
-import Logo from './module/1_Logo/Logo'
-import Avatar from './module/2_Avatar/Avatar'
-import NickName from './module/3_Nick_name/Nick_name'
-import Menu from './module/4_Menu_brg/Menu_block_flex'
+// import Logo from './module/1_Logo/Logo'
+// import Avatar from './module/2_Avatar/Avatar'
+// import NickName from './module/3_Nick_name/Nick_name'
+// import Menu from './module/4_Menu_brg/Menu_block_flex'
 
 
 
@@ -12,10 +12,10 @@ function App() {
 
     return (
         <div className='App'>
-            <Logo></Logo>
-            <Avatar></Avatar>
-            <NickName></NickName>
-            <Menu></Menu>
+            {/* <Logo></Logo> */}
+            {/* <Avatar></Avatar> */}
+            {/* <NickName></NickName> */}
+            {/* <Menu></Menu> */}
 
 
 
